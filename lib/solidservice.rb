76fa6b9c4c98e81f121db15408ea038bfafbdd03
Hopper Gee <hopper.gee@hey.com>
2022-04-30 23:11:22 +0800

    Add ActiveSupport dependency

--- lib/solidservice.rb
+++ lib/solidservice.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "solidservice/version"
+require "active_support/core_ext/hash/indifferent_access"
 
 module SolidService
   class Error < StandardError; end
